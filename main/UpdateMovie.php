464cb38a83ecc1629fa114df62db789310f6fce4
Lukas Komprs <komprs.l@gmail.com>
2018-08-17 16:32:18 +0200

    added field for choosing and laoding imgs to db, method saveImage and displaImages added

--- main/UpdateMovie.php
+++ main/UpdateMovie.php
@@ -36,27 +36,82 @@ move_uploaded_file($sourcePath,$targetPath) ;    // Moving Uploaded file
 ?>
 
 
-<!DOCTYPE html>
 <html>
 <head>
     <title>Select movie</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
-
 <body>
 
-
-<form action="" method="POST">
+      <form action="" method="POST">
                 <br> Title: <input type="text" name='title' value="<?php echo $title; ?>"><br>
 
-                <span> <?php echo $titleErr ?><span>
+
                 <br> Description <input type="text" name="description" value="<?php echo $description; ?>"><br>
 
                 <br> Year <input type="number" name="year" value="<?php echo $year; ?>"><br>
 
-                <br> Length <input type="number" name="length" value="<?php echo $length; ?>"><br>
+                <br> Length <input type="number" name="length" value="<?php echo $length; ?>"><br><br>
+                  <span><?php if($err){
+                    echo 'Fill the empty fields pls';
+                    }
+                  ?></span>
+                 <input type="submit" name="submit" value="submit" /><br>
+      </form> <br>
 
-                 <input type="submit" name="submit" value="submit" />
-            </form>
+      <h2> Save a new image</h2>
+      <?php
+        ini_set('mysql.connect_timeout',300);
+        ini_set('default_socket_timeout',300);
+       ?>
+      <form method="post" enctype="multipart/form-data">
+        <br/>
+          <input type="file" name="image" />
+          <br/> <br/>
+          <input type="submit" name="sub" value="upload" />
+      </form>
 
-</body>
+      <?php
+
+
+        if(isset($_POST['sub'])){
+          if(getimagesize($_FILES['image']['tmp_name']) == FALSE)
+          {
+              echo "Please selcet a picture";
+          }
+          else {
+            $image = addslashes($_FILES['image']['tmp_name']); // Storing source path of the file in a variable
+            $name = addslashes($_FILES['image']['name']);
+            $image =file_get_contents($image);
+            $image = base64_encode($image);
+            saveImage($name,$image);
+          }
+        }
+        displayImages();
+
+        //php script to store imgs
+        function saveImage($name,$image){
+          $query = new queryToDatabase();
+          $conn = $query->connect();
+          $qry="insert into images(name,image) values ('$name', '$image')";
+          $result = mysqli_query($conn,$qry);
+          if($result){
+            echo "<br/>Image uploaded";
+          } else {
+            echo "<br/>Image not uploaded";
+          }
+        }
+
+        function displayImages(){
+          $query = new queryToDatabase();
+          $conn = $query->connect();
+          $qry="select * from images";
+          $result= mysqli_query($conn,$qry);
+          while($row = mysqli_fetch_assoc($result)){
+              echo '<img height="300" width="300" src="data:image;base64,'.$row['image'].' "> <br/>';
+          }
+          mysqli_close($conn);
+        }
+      ?>
+  </body>
 </html>
