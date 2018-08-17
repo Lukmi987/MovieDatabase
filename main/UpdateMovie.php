122bc071044c724b3c414985857369ba43e07892
Lukas Komprs <komprs.l@gmail.com>
2018-08-17 16:20:08 +0200

    code moved to methods, update query, select query, verifyinsert

--- main/UpdateMovie.php
+++ main/UpdateMovie.php
@@ -1,80 +1,40 @@
-
-
 <?php
 
-include "../config/dbconn.php"; // include connection file
-include "../main/authentication.php"; // include connection file
+include "../main/queryToDatabase.php"; // include connection file
+ // include connection file
 
 $au = new authentication();
-$filmId= $_GET['id'];
-
-
-
-//definition of error variables
-
-$titleErr = $descriptionErr = $yearErr = $lengthErr= '';
+$query = new queryToDatabase();
 $err = false;
 
 if (isset($_POST['submit'])){
-
- $titleinfo=$au->testInput('title');
- $err= $titleinfo[1];
-
- $descriptioninfo=$au->testInput('description');
- $err=$descriptioninfo[1];
-
- $yearInfo=$au->testInput('year');
- $err=$yearInfo[1];
-
- $lengthInfo=$au->testInput('length');
- $err=$lengthInfo[1];
-
+  $err = $au->testInput();
   if(!$err) {
-  $title = $au->test_input($_POST['title']);
-  $description= $au->test_input($_POST['description']);
-  $release_year= $au->test_input($_POST['year']);
-  $length= $au->test_input($_POST['length']);
-
-$update =  "UPDATE
-              movies
-            SET
-              title='".$title."',
-              description='".$description."',
-              release_year='".$release_year."',
-              length='".$length."'
-           WHERE
-             film_id ='$filmId'";
-
-             $updateQuery = mysqli_query($conn,$update) or die(mysqli_error($conn));
-
-             if($updateQuery){
-               echo "<h3>The record has been updated.</h3>";
-             } else {
-               echo "<h3>The record has not been updated.</h3>";
-             }
-          }
-        }
-
-  $sql = "SELECT * FROM movies WHERE film_id='$filmId'";
-
-  $result = mysqli_query($conn, $sql) OR die(mysqli_error($conn));
-
-
-if ($result->num_rows > 0){
-  while($row= mysqli_fetch_assoc($result)) {
-
-$title = $row['title'];
-
-$description = $row['description'];
-$year = $row['release_year'];
-$length = $row['length'];
-
-
+  $update = $query->updateFilm();
+  $wasItsave = $query->verifyInsertSuccess($update);
+  echo $wasItsave;
   }
-} else {
-  echo "0 result";
 }
+
+$result = $query->selectFilm();
+//fill the variables for the input fields of the form with the current movie
+  if ($result->num_rows > 0){
+    while($row= mysqli_fetch_assoc($result)) {
+      $title = $row['title'];
+      $description = $row['description'];
+      $year = $row['release_year'];
+      $length = $row['length'];
+    }
+  } else {
+  echo "0 result";
+  }
+//php par for ajax, getting imgs from db
+$sourcePath = $_FILES['file']['tmp_name'];       // Storing source path of the file in a variable
+$targetPath = "upload/".$_FILES['file']['name']; // Target path where file is to be stored
+move_uploaded_file($sourcePath,$targetPath) ;    // Moving Uploaded file
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -87,19 +47,14 @@ $length = $row['length'];
 
 <form action="" method="POST">
                 <br> Title: <input type="text" name='title' value="<?php echo $title; ?>"><br>
-                <span><?php $err=$au->testInput('title');
 
-                 echo $err[0];?></span>
-
+                <span> <?php echo $titleErr ?><span>
                 <br> Description <input type="text" name="description" value="<?php echo $description; ?>"><br>
-                <span><?php $err=$au->testInput('description');
-                  echo $err[0];?></span>
+
                 <br> Year <input type="number" name="year" value="<?php echo $year; ?>"><br>
-                <span><?php $err=$au->testInput('year');
-                  echo $err[0];?></span>
+
                 <br> Length <input type="number" name="length" value="<?php echo $length; ?>"><br>
-                <span><?php $err=$au->testInput('length');
-                  echo $err[0];?></span>
+
                  <input type="submit" name="submit" value="submit" />
             </form>
 
